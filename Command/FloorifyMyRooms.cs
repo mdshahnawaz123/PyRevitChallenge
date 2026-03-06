e0b73fa508cb2ba00cf8fe62b9e4af771e4dfa6e
mdshahnawaz123 <41420658+mdshahnawaz123@users.noreply.github.com>
2026-03-06 14:56:35 +0400

    Room Floorify Added

---
+++ Command/FloorifyMyRooms.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace PyRevitChallenge.Command
+{
+    [Transaction(TransactionMode.Manual)]
+    public class FloorifyMyRooms : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uidoc = commandData.Application.ActiveUIDocument;
+            var doc = uidoc.Document;
+            try
+            {
+                var frm = new UI.FloorifyMyRooms(doc, uidoc);
+                frm.ShowDialog();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ UI/FloorifyMyRooms.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace PyRevitChallenge.UI
+{
+    public partial class FloorifyMyRooms : Window
+    {
+        private Document doc;
+        private UIDocument uidoc;
+
+        private List<Room> selectedRooms = new List<Room>();
+
+        public FloorifyMyRooms(Document doc, UIDocument uidoc)
+        {
+            InitializeComponent();
+            this.doc = doc;
+            this.uidoc = uidoc;
+        }
+
+        // SELECT ROOMS
+        private void btnSelectRooms(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            var picked = uidoc.Selection
+                .PickElementsByRectangle(new RoomFilter(), "Select Rooms");
+
+            selectedRooms = picked
+                .Select(x => x as Room)
+                .Where(x => x != null)
+                .ToList();
+
+            MessageBox.Show(selectedRooms.Count + " Rooms Selected");
+            this.Show();
+        }
+
+        // CREATE FLOORS
+        private void btnFloorify(object sender, RoutedEventArgs e)
+        {
+            if (selectedRooms.Count == 0)
+            {
+                MessageBox.Show("Please select rooms first.");
+                return;
+            }
+
+            using (Transaction t = new Transaction(doc, "Create Floors From Rooms"))
+            {
+                t.Start();
+
+                foreach (Room room in selectedRooms)
+                {
+                    var boundary = room.GetBoundarySegments(new SpatialElementBoundaryOptions());
+
+                    if (boundary == null) continue;
+
+                    foreach (var loop in boundary)
+                    {
+                        List<Curve> curves = new List<Curve>();
+
+                        foreach (var seg in loop)
+                        {
+                            curves.Add(seg.GetCurve());
+                        }
+
+                        var curveLoop= new CurveLoop();
+
+                        foreach (var c in curves)
+                        {
+                            curveLoop.Append(c);
+                        }
+                        List<CurveLoop> loops = new List<CurveLoop>();
+                        loops.Add(curveLoop);
+
+
+
+                        FloorType floorType = new FilteredElementCollector(doc)
+                            .OfClass(typeof(FloorType))
+                            .Cast<FloorType>()
+                            .First();
+
+                        Level level = doc.GetElement(room.LevelId) as Level;
+
+                        var lvlid = level.LevelId;
+
+                        Floor.Create(doc, loops, floorType.Id, lvlid);
+                    }
+                }
+
+                t.Commit();
+            }
+
+            MessageBox.Show("Floors Created Successfully");
+        }
+
+        // ROOM FILTER
+        public class RoomFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element elem)
+            {
+                return elem is Room;
+            }
+
+            public bool AllowReference(Reference reference, XYZ position)
+            {
+                return false;
+            }
+        }
+    }
+}
